eab6a763994ecc134f9ff32b864f2cb52102502a
jakezenger <jakezenger@gmail.com>
2019-03-08 14:59:32 -0800

    added Calculator.sln

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        public static float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+
+        public static float Divide(float a, float b)
+        {
+            return a / b;
+        }
+
+        public static float Add(float a, float b)
+        {
+            return a + b;
+        }
+
+        public static float Subtract(float a, float b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void DivideTest()
+        {
+            float num1 = 2.6f, num2 = 3.5f;
+
+            float expected = num1 / num2;
+            float actual = Calculator.Divide(num1, num2);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
